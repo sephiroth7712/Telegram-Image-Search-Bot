25a3b7e1b2c1fa5392a63dc27f59ce7a1fae3614
sephiroth7712 <roshanjames7@gmail.com>
2018-11-06 02:37:35 +0530

    Changed in comments

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.post('/new-message', function(req, res) {
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
   if (!message || message.text.toLowerCase().indexOf('hi') < 0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
+    // In case a message is not present, or if our message does not have the word hi in it, do nothing and return an empty response
     return res.end()
   }
 
-  // If we've gotten this far, it means that we have received a message containing the word "hi.
+  // If we've gotten this far, it means that we have received a message containing the word "hi".
   // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Please die <telegram name>!"
   // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
   axios
